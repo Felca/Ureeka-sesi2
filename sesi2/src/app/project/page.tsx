b70d3dea141c061999426191ad98b2ad4fb6531a
Felca <felicia.rfs.plg@gmail.com>
2025-05-22 12:07:50 +0700

    added project page

--- sesi2/src/app/project/page.tsx
+++ sesi2/src/app/project/page.tsx
@@ -3,22 +3,10 @@ import Image from "next/image";
 export default function Project() {
   return (
     <div className="grid grid-cols-2 gap-2 m-4">
-        {/* <div className="m-4 border p-4 transition ease-in-out group hover:border-red-300">
-          <img
-            className="mx-auto group-hover:scale-90 transform transition ease-in-out"
-            src="https://placehold.co/400x300/white/black"
-            width={400}
-            height={300}
-          />
-          <h1 className="mt-5 font-bold group-hover:text-red-300 transition">Lorem Project 1</h1>
-          <p className="group-hover:text-red-300 transition">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div> */}
-
         <div className="m-4 border p-4 transition ease-in-out group hover:brightness-75">
           <img className="mx-auto"
-            src="https://placehold.co/400x300/white/black"
-            width={400}
+            src="https://placehold.co/300x200/white/black"
+            width={500}
             height={300}>
           </img>
             <h1 className="mt-5 font-bold">Lorem Project 1</h1>
@@ -27,8 +15,8 @@ export default function Project() {
 
         <div className="m-4 border-1 p-4">
           <img className="mx-auto"
-            src="https://placehold.co/400x300/white/black"
-            width={400}
+            src="https://placehold.co/300x200/white/black"
+            width={500}
             height={300}>
           </img>
             <h1 className="mt-5 font-bold">Lorem Project 1</h1>
@@ -37,8 +25,8 @@ export default function Project() {
         
         <div className="m-4 border-1 p-4">
           <img className="mx-auto"
-            src="https://placehold.co/400x300/white/black"
-            width={400}
+            src="https://placehold.co/300x200/white/black"
+            width={500}
             height={300}>
           </img>
             <h1 className="mt-5 font-bold">Lorem Project 1</h1>
@@ -47,8 +35,8 @@ export default function Project() {
         
         <div className="m-4 border-1 p-4">
           <img className="mx-auto"
-            src="https://placehold.co/400x300/white/black"
-            width={400}
+            src="https://placehold.co/300x200/white/black"
+            width={500}
             height={300}>
           </img>
             <h1 className="mt-5 font-bold">Lorem Project 1</h1>
---
+++ sesi2/src/app/skills/page.tsx
@@ -0,0 +1,12 @@
+export default function skills(){
+    return(
+        <div>
+            <div className="w-30 h-30 flex border-1 origin-center rotate-45 translate-10">
+                <p className="m-auto font-bold">Web Dev</p>
+            </div>
+            <div className="w-30 h-30 flex border-1 skew-y-3 hover:bg-amber-300">
+                <p className="m-auto font-bold">Web Dev</p>
+            </div>
+        </div>
+    )
+}
